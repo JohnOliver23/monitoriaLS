9adc2beb712ea3a140cd3017a86b04c3e4aa7e08
John Oliver <johnoliver021@gmail.com>
2018-04-12 00:10:43 -0300

    Create arrays.js

---
+++ loop em array/loops/arrays.js
@@ -0,0 +1,32 @@
+function doubleNumber(array){
+  //TO DO
+}
+
+function ftEven(array) {
+  //TO DO
+}
+
+function ftOdd(array) {
+  //TO DO
+}
+
+function sumArray(array) {
+  //TO DO
+}
+
+function mtArray(array) {
+  //TO DO
+}
+
+function findNumber(array) {
+  //TO DO
+}
+
+module.exports = {
+  doubleNumber,
+  ftEven,
+  ftOdd,
+  sumArray,
+  mtArray,
+  findNumber
+};
