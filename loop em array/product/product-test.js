83f578cfe44a56456b3d6501db7d19ee11ab03e6
John Oliver <johnoliver021@gmail.com>
2018-04-12 11:25:59 -0300

    Create product-test.js

---
+++ loop em array/product/product-test.js
@@ -0,0 +1,22 @@
+const cart = require('./product');
+describe('cart Tool', () => {
+  const produtos = [
+    {
+      nome: 'Bicicleta',
+      preco: 1200.0
+    },
+    {
+      nome: 'Capacete',
+      preco: 450.0
+    }
+  ]
+  it('counting total cart to be 1650', () => {
+    expect(cart.total(produtos)).toBe(1650.0);
+  });
+  
+  xit('filtering products with a price greater than 1000', () => {
+    expect(cart.gtThousand(produtos)).toEqual(['Bicicleta']);
+  });
+  
+  
+});
