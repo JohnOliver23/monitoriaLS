59bad3866984c90d1a53f5f5b0936b662e3418b1
Paulo Henrique <ph.equfpb@gmail.com>
2018-04-20 11:13:33 -0300

    Consertando testes

--- strings/string.test.js
+++ strings/string.test.js
@@ -19,11 +19,11 @@ describe('Strings Tests:', () => {
     });
 
     test.skip("Counting words it is easier than counting stars!", () => {
-        expect(stringsFunctions.countingWords('With great power, comes great responsibility.').toBe(6));
+        expect(stringsFunctions.countingWords('With great power, comes great responsibility.')).toBe(6);
     });
 
     test.skip("I\'m pro hacker!", () => {
-        expect(stringsFunctions.amazingHacker("Nory was a Catholic because her mother was a Catholic, and Nory\’s mother was a Catholic because her father was a Catholic, and her father was a Catholic because his mother was a Catholic, or had been.", 'Catholic', 'Hacker').toEqual("Nory was a Hacker because her mother was a Hacker, and Nory\’s mother was a Hacker because her father was a Hacker, and her father was a Hacker because his mother was a Hacker, or had been."));
+        expect(stringsFunctions.amazingHacker("Nory was a Catholic because her mother was a Catholic, and Nory\’s mother was a Catholic because her father was a Catholic, and her father was a Catholic because his mother was a Catholic, or had been.", 'Catholic', 'Hacker')).toEqual("Nory was a Hacker because her mother was a Hacker, and Nory\’s mother was a Hacker because her father was a Hacker, and her father was a Hacker because his mother was a Hacker, or had been.");
     });
 
 })
