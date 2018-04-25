c6ac24aa7d89e58d8b23852a8119acee381d665f
John Oliver <johnoliver021@gmail.com>
2018-04-24 23:42:38 -0300

    Create comics.js

---
+++ Json/comics/comics.js
@@ -0,0 +1,24 @@
+function namesByComics(characters, text){
+  //TO DO
+}
+
+function weight(characters, text){
+  //TO DO
+
+}
+function height(characters, text){
+  //TO DO
+}
+
+function topHeightNames(characters, text){
+  //TO DO
+
+}
+function topWeightNames(characters, text){
+  // TO DO
+
+}
+
+
+
+module.exports = {namesByComics, weight, topWeightNames, height,topHeightNames }
