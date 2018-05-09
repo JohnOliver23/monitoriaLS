d6abf6b150e042d5298c5e1216e5b0b5efcc0dfa
John Oliver <johnoliver021@gmail.com>
2018-05-08 23:38:58 -0300

    Create examstats.js

---
+++ Objeto/exam-oo/examstats.js
@@ -0,0 +1,21 @@
+module.exports = class ExamStats{
+  constructor(grades){
+    //TO DO
+  }
+  avg(){
+    //TO DO
+  }
+  min(number=1){
+    //TO DO
+  }
+  max(number = 1){
+    //TO DO
+  }
+  lt(point){
+    //TO DO
+  }
+  gt(point){
+    //TO DO
+    
+  }
+}
